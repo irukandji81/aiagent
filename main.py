4f575989d0c4efecb4095fa35f6c4fdf9e00af06
irukandji81 <nathan.crawford81@gmail.com>
2025-06-11 12:33:00 -0700

    Update

--- main.py
+++ main.py
@@ -9,12 +9,40 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 # Define system prompt
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+system_prompt = """You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons."""
 
 # Ensure a prompt is provided
 if len(sys.argv) < 2:
     print("Error: Please provide a prompt as a command-line argument.")
     sys.exit(1)
+
+# Define the Function Schema
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+# Define the available functions for the AI to call
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 # Extract command-line arguments
 user_prompt = sys.argv[1]
@@ -32,8 +60,17 @@ messages = [
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    config=types.GenerateContentConfig(
+        system_instruction=system_prompt,
+        tools=[available_functions],
+    )
 )
+
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(response.text)
 
 # Print verbose output first if enabled
 if verbose:
